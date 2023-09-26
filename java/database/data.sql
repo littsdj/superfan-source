7610247db66fc9f630ded120d7a8a6abf9bb86b6
Lindsey Haney <lhaney0702@gmail.com>
2023-09-26 11:55:28 -0400

    Added genres to data.sql file.

--- java/database/data.sql
+++ java/database/data.sql
@@ -3,4 +3,15 @@ BEGIN TRANSACTION;
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
 
+INSERT INTO genres (genre_name) VALUES ('Pop');
+INSERT INTO genres (genre_name) VALUES ('Rock');
+INSERT INTO genres (genre_name) VALUES ('Country');
+INSERT INTO genres (genre_name) VALUES ('Jazz');
+INSERT INTO genres (genre_name) VALUES ('Electronic');
+INSERT INTO genres (genre_name) VALUES ('Hip-Hop');
+INSERT INTO genres (genre_name) VALUES ('World');
+INSERT INTO genres (genre_name) VALUES ('Experimental');
+INSERT INTO genres (genre_name) VALUES ('Latin');
+INSERT INTO genres (genre_name) VALUES ('Metal');
+
 COMMIT TRANSACTION;
