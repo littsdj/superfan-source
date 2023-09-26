01c06068db2bcb6829c023a31f7f6a718bc540c8
Dj Litts <littsdj@gmail.com>
2023-09-26 11:51:34 -0500

    added a genre drop down selector

--- vue/src/components/NewBandForm.vue
+++ vue/src/components/NewBandForm.vue
@@ -5,7 +5,19 @@
             <label for="bandName">Band Name</label>
             <input type="text" name="bandName">
             <textArea rows="8" cols="45" ></textArea>
-      
+            <select name="genre" id="genrePicker">
+                <option value="">--select a genre--</option>
+                <option value="pop">Pop</option>
+                <option value="country">Country</option>
+                <option value="rock">Rock</option>
+                <option value="jazz">Jazz</option>
+                <option value="electronic">Electronic</option>
+                <option value="hip-hop">Hip-Hop</option>
+                <option value="world">World</option>
+                <option value="experimental">Experimental</option>
+                <option value="latin">Latin</option>
+                <option value="metal">Metal</option>
+            </select>
         </form>
     </div>
 </template>
