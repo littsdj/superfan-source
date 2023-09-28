215a28fe976b335e412cb20cc105b302ceb83ccc
Dylan Roberts <flapjackage@gmail.com>
2023-09-28 10:22:06 -0400

    Styling for sidebar and titlebar

--- vue/src/components/sidebar.vue
+++ vue/src/components/sidebar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="sidebar">
-    <div class="navLink">  
+    <div class="navLink">
       <router-link v-bind:to="{ name: 'home' }">Home</router-link>
     </div>
     <div class="navLink">
@@ -10,47 +10,53 @@
       <a href="">My Bands</a>
     </div>
     <div class="navLink">
-      <router-link v-bind:to="{ name: 'newBand'}">New Band</router-link>
+      <router-link v-bind:to="{ name: 'newBand' }">New Band</router-link>
     </div>
     <div class="navLink">
       <a href="">My account</a>
     </div>
     <div class="navLink">
-      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
+      <router-link
+        v-bind:to="{ name: 'logout' }"
+        v-if="$store.state.token != ''"
+        >Logout</router-link
+      >
     </div>
   </div>
-    
 </template>
 
 <script>
-export default {
-    
-}
+export default {};
 </script>
 
 <style scoped>
-  .navLink{
-    padding-top:15px;
-    padding-bottom: 15px;
-    border: ;
-  }
-  .sidebar {
+.sidebar {
   display: flex;
-  flex: 1 0 0; 
-  background-color: whitesmoke;
+  flex: 1 0 0;
+  background-color: rgb(245, 245, 245, 0.9);
   padding: 20px;
   flex-direction: column;
-  opacity: 0.8;
   margin: 20px;
-  justify-content: around;
+  justify-content: space-around;
 }
-    .sidebar *{
-      text-align: center;
-      align-items:  center;
+.navLink {
+  padding-top: 15px;
+  padding-bottom: 15px;
+  text-align: center;
+  font-size: 20pt;
+  font-weight: bolder;
+  font-family: fantasy;
+  opacity: 1;
+  background-color: hotpink;
+  border-radius: 5px;
+  border-color: black;
+}
+.sidebar * {
+  text-align: center;
+  align-items: center;
 
-      text-decoration: none;
-      /* flex-basis: 100% ; */
-      max-height: 100px;
-
-    }
+  text-decoration: none;
+  /* flex-basis: 100% ; */
+  max-height: 100px;
+}
 </style>
