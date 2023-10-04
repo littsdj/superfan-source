241dc2955fc6073f1d8d54992a96a8ec1c4f2076
Dj Litts <littsdj@gmail.com>
2023-10-04 09:10:48 -0500

    added images to band cards

--- vue/src/components/BandPage.vue
+++ vue/src/components/BandPage.vue
@@ -84,7 +84,6 @@ export default {
       if (this.band.bandImage && this.band.bandImage.fileName) {
         const dotIndex = this.band.bandImage.fileName.lastIndexOf(".");
         const extension = this.band.bandImage.fileName.substring(dotIndex + 1);
-        //return `data:image/${extension};base64, ${this.bandImage.imageData}`;
         return `data:image/${extension};base64, ${this.band.bandImage.imageData}`;
       }
       return "";
