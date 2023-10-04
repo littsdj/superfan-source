a09cc6d741b2feca1c29a0b5b51a9afbddaa0a46
Dj Litts <littsdj@gmail.com>
2023-10-04 15:57:49 -0500

    added message sorting functionality. I think it works pretty well, but haven't done much for hard testing. great job team!

--- vue/src/components/Inbox.vue
+++ vue/src/components/Inbox.vue
@@ -2,7 +2,20 @@
   <div id="content">
     <div class="inbox">
       <h1 id="pageHeaderTitle">Messages From Bands You Follow</h1>
-      <div class="messageBox" v-for="message in messages" v-bind:key="message.messageId"> 
+      <select name="alphabetical" id="alphabetical" v-model.number="orderSelection">
+        <option value="1">Newest First</option>
+        <option value="2">Oldest First</option>
+        
+      </select>
+      <select name="band-picker" id="band-picker" v-model="filterBandId">
+        <option value="0">Filter By Band</option>
+        <option v-for="band in followedBands" v-bind:key="band.bandId" :value="band.bandId">{{band.bandName}}</option>
+
+
+
+
+      </select>
+      <div class="messageBox" v-for="message in displayedMessages" v-bind:key="message.messageId"> 
         <message-box v-bind:message="message" />
       </div>
     </div>
@@ -10,19 +23,51 @@
 </template>
 
 <script>
+import BandService from "../services/BandService.js";
 import MessageService from "../services/MessageService.js";
 import MessageBox from "../components/MessageBox.vue";
 export default {
   components: { MessageBox },
   data() {
     return {
-      messages: []
+      messages: [],
+      followedBands: [],
+      filterBandId: 0,
+      orderSelection: 1
     }
+  },
+  computed: {
+    displayedMessages() {
+      let filteredMessages = this.messages;
+      if (this.filterBandId != 0){    //picks a bandId from drop down, and only shows those bands
+         filteredMessages = this.messages.filter( message => {
+          return (this.filterBandId === message.senderBandId);
+        })
+      }
+      if (this.orderSelection === 1){
+        return filteredMessages;
+      }
+      if (this.orderSelection === 2){
+        return filteredMessages.reverse();
+      }
+      return filteredMessages
+    },
   },
   created() {
     MessageService.getAllMessagesToUser(this.$store.state.user.id).then( (response) => {
-      this.messages = response.data
-    } )
+      this.messages = response.data;
+    })
+    BandService.getAllUserFollowing(this.$store.state.user.id).then( response => {
+      this.followedBands = response.data;
+    })
+
+  },
+  methods: {
+    getBandNameByBandId(bandId){
+      BandService.getBandByBandId(bandId).then( response => {
+        return response.data.bandName;
+      })
+    }
   }
 };
 </script>
